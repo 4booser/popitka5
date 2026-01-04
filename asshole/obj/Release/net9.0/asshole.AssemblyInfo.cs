abe5cc17bc748710b3fc2a7d0b4ae6a006e17fb3
4booser <snusogriz@gmail.com>
2026-01-05 01:56:48 +0200

    car fact vmesto weather

--- asshole/obj/Release/net9.0/asshole.AssemblyInfo.cs
+++ asshole/obj/Release/net9.0/asshole.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asshole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4755a5d36c6a5f7bfbf082a7a5ef64cea2ecac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b122a9b0fcf8a786d85f52f28fbb0ba315b7244c")]
 [assembly: System.Reflection.AssemblyProductAttribute("asshole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asshole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
